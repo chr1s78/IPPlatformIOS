f2f3cb8451372ffb79f46b588840563dacc94183
chr1s78 <ayahirano2014@126.com>
2021-08-19 15:03:49 +0800

    Initial Commit

---
+++ IPPlatformIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IPPlatformIOS
+//
+//  Created by Chr1s on 2021/8/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IPPlatformIOS/IPPlatformIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPPlatformIOSApp.swift
+//  IPPlatformIOS
+//
+//  Created by Chr1s on 2021/8/19.
+//
+
+import SwiftUI
+
+@main
+struct IPPlatformIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
